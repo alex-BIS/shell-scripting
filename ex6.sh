24c383a9302bd4abe9cd4341fc4a2c86589e15c9
alex.craven <alex.craven@bluesmithis.com>
2021-07-28 12:31:47 +0100

    further bugfixes for ex6

--- ex6.sh
+++ ex6.sh
@@ -2,7 +2,12 @@
 echo "Please enter your surname:"
 read SNAME
 echo
+
+# checking if surname entered contains chars in "Ssurname"
+# if not, print the column headers otherwise it will do it on its own thru grep
 if [ ! $SNAME == [Ssurname] ] ; then
   echo "Name|Surname|Email|Phone"
 fi
+
+# searching address book for entered surname
 grep \|[a-zA-Z]*${SNAME}[a-zA-Z]*\| addressBook.txt
