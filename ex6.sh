d4cd3051ea61649e19a4b902d84877472c9a1f19
alex.craven <alex.craven@bluesmithis.com>
2021-07-28 12:25:49 +0100

    modified ex6.sh to remove bugs

--- ex6.sh
+++ ex6.sh
@@ -2,5 +2,7 @@
 echo "Please enter your surname:"
 read SNAME
 echo
-echo "Name | Surname | Email | Phone"
-grep "$SNAME" addressBook.txt
+if [ ! $SNAME == [Ssurname] ] ; then
+  echo "Name|Surname|Email|Phone"
+fi
+grep \|[a-zA-Z]*${SNAME}[a-zA-Z]*\| addressBook.txt
