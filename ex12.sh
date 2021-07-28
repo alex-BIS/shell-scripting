94aa599b71e0a14e61f3b42a58ee31be81ea7041
alex.craven <alex.craven@bluesmithis.com>
2021-07-28 16:13:07 +0100

    added comments to ex12 for comprehension of program function

--- ex12.sh
+++ ex12.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+
+# displays username in []
 echo -n "What is your name [ `whoami` ] "
 read myname
+
+# if input is nothing, set var = `whoami`
 if [ -z "$myname" ]; then
   myname=`whoami`
 fi
+
+# print $myname
 echo "Your name is : $myname"
+
+# print $myname or if it has no value, print username
 echo "Your name is : ${myname:-`whoami`}"
